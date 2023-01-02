1f496af4822c096c4989b4fb9b81a09f0248db1f
Бойко Боев <boykopetevboev@gmail.com>
2023-01-02 22:04:19 +0200

    Tutorial 3

--- TensorFlow/youtube-tutorial/2-Neural-Networks-with-Sequential-and-Functional-API.py
+++ TensorFlow/youtube-tutorial/2-Neural-Networks-with-Sequential-and-Functional-API.py
@@ -1,2 +1,51 @@
-import tensorflow as ts
+import sys
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.datasets import mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.reshape(-1, 28*28).astype("float32") / 255.0
+x_test = x_test.reshape(-1, 28*28).astype("float32") / 255.0
+print(x_train.shape)
+
+# Sequential API
+model = keras.Sequential([
+    keras.Input(shape=(28*28)),
+    layers.Dense(512, activation="relu"),
+    layers.Dense(256, activation="relu"),
+    layers.Dense(10),
+])
+model= keras.Sequential()
+model.add(keras.Input(shape=(28*28)))
+model.add(layers.Dense(512, activation="relu"))
+model.add(layers.Dense(256, activation="relu"))
+model.add(layers.Dense(10))
+print(model.summary())
+sys.exit()
+
+model.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=keras.optimizers.Adam(learning_rate=0.001),
+    metrics=["accuracy"],
+)
+model.fit(x_train, y_train, batch_size=32, epochs=5, verbose=2)
+model.evaluate(x_test, y_test, batch_size=32, verbose=2)
+ 
+# Functional API
+inputs = keras.Input(shape=(784))
+x = layers.Dense(512, activation="relu")(inputs)
+x = layers.Dense(256, activation="relu")(x)
+outputs = layers.Dense(10, activation="softmax")(x)
+model = keras.Model(inputs=inputs, outputs=outputs)
+print(model.summary())
+
+model.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=keras.optimizers.Adam(learning_rate=0.001),
+    metrics=["accuracy"],
+)
+model.fit(x_train, y_train, batch_size=32, epochs=5, verbose=2)
+model.evaluate(x_test, y_test, batch_size=32, verbose=2)
 
