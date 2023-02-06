7076881362feebcb0a3fadd2158526ea48c71658
Бойко Боев <boyko.boev@mentormate.com>
2023-02-06 19:35:06 +0200

    Adding Regularization with L2 and Dropout

--- TensorFlow/youtube-tutorial/3-Convolutional-Neural-Networks-with-Sequential-and-Functional-API.py
+++ TensorFlow/youtube-tutorial/3-Convolutional-Neural-Networks-with-Sequential-and-Functional-API.py
@@ -1,7 +1,7 @@
 import sys
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
+from tensorflow.keras import layers, regularizers
 from tensorflow.keras.datasets import cifar10
 
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
@@ -24,39 +24,50 @@ model = keras.Sequential([
 print(model.summary())
 
 # Functional API
+
+
 def my_model():
     inputs = keras.Input(shape=(32, 32, 3))
 
-    x = layers.Conv2D(32, 3)(inputs)
+    x = layers.Conv2D(
+        32, 3, padding="same", kernel_regularizer=regularizers.l2(0.01)
+    )(inputs)
     x = layers.BatchNormalization()(x)
     x = keras.activations.relu(x)
-    
+
     x = layers.MaxPooling2D()(x)
-    
-    x = layers.Conv2D(64, 5, padding="same")(x)
+
+    x = layers.Conv2D(
+        64, 5, padding="same", kernel_regularizer=regularizers.l2(0.01)
+    )(x)
     x = layers.BatchNormalization()(x)
     x = keras.activations.relu(x)
-    
-    x = layers.Conv2D(128, 3)(x)
+
+    x = layers.Conv2D(
+        128, 3, padding="same", kernel_regularizer=regularizers.l2(0.01)
+    )(x)
     x = layers.BatchNormalization()(x)
     x = keras.activations.relu(x)
 
     x = layers.Flatten()(x)
-    x = layers.Dense(64, activation="relu")(x)
+    x = layers.Dense(
+        64, activation="relu", kernel_regularizer=regularizers.l2(0.01)
+    )(x)
+    x = layers.Dropout(0.5)(x)
 
     outputs = layers.Dense(10)(x)
     model = keras.Model(inputs=inputs, outputs=outputs)
     return model
 
+
 model = my_model()
 print(model.summary())
 
 model.compile(
-    loss = keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
     optimizer=keras.optimizers.Adam(learning_rate=3e-4),
     metrics=["accuracy"]
 )
 
 model.fit(x_train, y_train, batch_size=64, epochs=10, verbose=2)
 model.evaluate(x_test, y_test, batch_size=64, verbose=2)
-
